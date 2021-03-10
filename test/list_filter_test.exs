39cbffc44ec22c616f223db13431eca36cd06614
Michel Victor <michelvictor.doc@gmail.com>
2021-03-09 23:00:46 -0300

    function to return the number of odd numbers

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,7 @@ defmodule ListFilterTest do
   use ExUnit.Case
 
   describe "call/1" do
-    test "returns the odd number" do
+    test "returns the number of odd numbers" do
       list = ["1", "3", "6", "43", "banana", "6", "abc"]
 
       response = ListFilter.call(list)
